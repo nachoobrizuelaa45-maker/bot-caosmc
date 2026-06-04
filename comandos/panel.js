b804a6595df02ee103d7b0e01b49dd68c4b969f4
nachoobrizuelaa45-maker <nachoobrizuelaa45@gmail.com>
2026-06-04 00:45:06 -0300

    Update panel.js

--- comandos/panel.js
+++ comandos/panel.js
@@ -3,19 +3,33 @@ const { ActionRowBuilder, ButtonBuilder, ButtonStyle, EmbedBuilder } = require('
 module.exports = {
     name: 'panel',
     execute(message, args) {
-        // Embed del panel
         const embed = new EmbedBuilder()
-            .setTitle('🔎• Seleccióne una 🗒️ categoría:')
-            .setColor(0x00FF00)
+            .setTitle('🔎 • Seleccióne una categoría • 🗒️')
+            .setColor(0xFF0000) // Borde rojo
             .setDescription(
-                'ℹ️• **Dudas**: Necesitas resolver tus dudas.\n' +
-                '📁• **Reportar usuario**: Reportar a un usuario que ha incumplido las normas.\n' +
-                '📋• **Reportar staff**: Reportar a un staff que no está haciendo lo correcto.\n' +
-                '📺• **Creador de Contenido**: Reclama recompensa como creador.\n' +
-                '📛• **Apelar ban**: ¿Baneado injustamente o segunda oportunidad?\n' +
-                '🆘• **Soporte global**: Tienes un problema y necesitas ayuda.\n' +
-                '🏆• **Compra Exclusiva**: Comprar kit o Rango VIP.'
-            );
+                'ℹ️ • **Dudas**\n' +
+                'Necesitas resolver tus dudas.\n\n' +
+                
+                '📁 • **Reportar usuario**\n' +
+                'Reportar a un usuario que ha incumplido las normas.\n\n' +
+                
+                '📋 • **Reportar staff**\n' +
+                'Reportar a un staff que no está haciendo lo correcto.\n\n' +
+                
+                '📺 • **Creador de Contenido**\n' +
+                'Reclama recompensa como creador.\n\n' +
+                
+                '📛 • **Apelar ban**\n' +
+                '¿Baneado injustamente o segunda oportunidad?\n\n' +
+                
+                '🆘 • **Soporte global**\n' +
+                'Tienes un problema y necesitas ayuda.\n\n' +
+                
+                '🏆 • **Compra Exclusiva**\n' +
+                'Comprar kit o Rango VIP.'
+            )
+            .setImage('https://cdn.discordapp.com/attachments/1480431171069284352/1511937757537370202/Screenshot_20260604_003811_Google.jpg?ex=6a224543&is=6a20f3c3&hm=6cac616ee56e0705f337d2c925dcd22859010a747d29279efd040b43f7960c5b&')
+            .setFooter({ text: '• Equipo de Soporte Administración CAOSMC' });
 
         // Fila 1 de botones
         const row1 = new ActionRowBuilder().addComponents(
@@ -35,4 +49,3 @@ module.exports = {
         message.channel.send({ embeds: [embed], components: [row1, row2] });
     }
 };
-            
