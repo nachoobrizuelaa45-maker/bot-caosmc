44981fbc9e1e4550faff796079f009070b9b98a6
nachoobrizuelaa45-maker <nachoobrizuelaa45@gmail.com>
2026-06-06 09:33:26 -0300

    Update setverificacion.js

--- comandos/setverificacion.js
+++ comandos/setverificacion.js
@@ -9,24 +9,23 @@ module.exports = {
 
         const embed = new EmbedBuilder()
             .setTitle("🔒 • Sistema de Verificación CAOSMC 🚀")
-            .setColor(0x00FF00) // Borde izquierdo en color verde
+            .setColor(0x00FF00) // Borde verde
             .setDescription(
                 "🎉 • ¡Bienvenido a la comunidad CAOSMC!\n\n" +
                 "Para acceder a todos nuestros canales, recibir noticias, y estar al tanto de las últimas actualizaciones, solo debes hacer clic en (VERIFICATE) y obtendrás el rol de  👋 • nuevo . Únete y disfruta de todos los beneficios que te ofrecemos. 🚀"
             )
-            .setImage('https://cdn.discordapp.com/attachments/1480431171069284352/1512654936310284288/Picsart_26-06-06_00-08-11-550.jpg?ex=6a24e130&is=6a238fb0&hm=a6a12d9875cdc8e8382c88fd6b9dbcda0722ad6cab37d2ba1fd22e3778001aec&') // Reemplazá esto por la URL de tu imagen
+            .setImage('https://cdn.discordapp.com/attachments/1480431171069284352/1512654936310284288/Picsart_26-06-06_00-08-11-550.jpg?ex=6a24e130&is=6a238fb0&hm=a6a12d9875cdc8e8382c88fd6b9dbcda0722ad6cab37d2ba1fd22e3778001aec&')
             .setFooter({ text: '📢 •Gracias por ser parte de CAOSMC | Equipo de Soporte 💻' });
 
         const row = new ActionRowBuilder().addComponents(
             new ButtonBuilder()
                 .setCustomId('verificar_caosmc')
                 .setLabel('Verificate')
-                .setStyle(ButtonStyle.Secondary)
-                .setEmoji('<:emoji_58:1512619522161508432>') // Tu emoji personalizado integrado
+                .setStyle(ButtonStyle.Success) // Ahora es verde (Success)
+                .setEmoji('1512619522161508432') // ID directo para mejor compatibilidad
         );
 
         message.channel.send({ embeds: [embed], components: [row] });
         message.delete().catch(() => {});
     }
 };
-    
