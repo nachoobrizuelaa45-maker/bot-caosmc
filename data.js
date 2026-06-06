5fa9f9fb6b6f0570150df66977e5bb6d14330745
nachoobrizuelaa45-maker <nachoobrizuelaa45@gmail.com>
2026-06-06 13:45:53 -0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,23 @@
+module.exports = {
+    trabajos: [
+        { nombre: 'Agricultor', req: 0 },
+        { nombre: 'Pescador', req: 50 },
+        { nombre: 'Carnicero', req: 100 },
+        { nombre: 'Cocinero', req: 150 },
+        { nombre: 'Repartidor', req: 250 },
+        { nombre: 'Taxista', req: 350 },
+        { nombre: 'Camionero', req: 500 },
+        { nombre: 'Basurero', req: 600 },
+        { nombre: 'Mecanico', req: 700 },
+        { nombre: 'Electricista', req: 800 },
+        { nombre: 'Conductor', req: 900 },
+        { nombre: 'Piloto', req: 1000 },
+        { nombre: 'Minero', req: 1200 },
+        { nombre: 'Medico', req: 1400 },
+        { nombre: 'Policia', req: 1500 },
+        { nombre: 'Armero', req: 2000 },
+        { nombre: 'Traficante', req: 2500 },
+        { nombre: 'Sicario', req: 3000 }
+    ]
+};
+
