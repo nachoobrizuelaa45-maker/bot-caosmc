cc43ee4a66f99a4016681e36cc671f3385eebdf3
nachoobrizuelaa45-maker <nachoobrizuelaa45@gmail.com>
2026-06-06 20:37:50 -0300

    Update verificarCanal.js

--- economia/verificarCanal.js
+++ economia/verificarCanal.js
@@ -2,13 +2,23 @@
 module.exports = {
     esCanalValido(message) {
         const CANAL_ID = '1512815434188062922'; // Tu canal de economía
-        if (message.channel.id !== CANAL_ID) {
-            message.delete().catch(() => {}); // Borra el comando si podés
-            message.reply(`💰• <@${message.author.id}> Este no es el canal de <#1512815434188062922>`).then(msg => {
-                setTimeout(() => msg.delete(), 5000); // Borra el aviso después de 5 segundos
-            });
-            return false;
+
+        // Si el mensaje está en el canal correcto, devolvemos true
+        if (message.channel.id === CANAL_ID) {
+            return true;
         }
-        return true;
+
+        // Si no está en el canal correcto:
+        // 1. Intentamos borrar el mensaje del usuario
+        message.delete().catch(() => {});
+
+        // 2. Enviamos el aviso y lo borramos a los 5 segundos
+        message.channel.send(`💰• <@${message.author.id}>, este no es el canal de economía. Por favor, usá <#1512815434188062922>.`)
+            .then(msg => {
+                setTimeout(() => msg.delete().catch(() => {}), 5000);
+            })
+            .catch(() => {});
+
+        return false;
     }
 };
