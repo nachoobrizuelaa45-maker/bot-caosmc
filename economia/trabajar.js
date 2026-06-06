504fc388bb7deafe02852d1c2e9cdea71237ee38
nachoobrizuelaa45-maker <nachoobrizuelaa45@gmail.com>
2026-06-06 16:47:01 -0300

    Update trabajar.js

--- economia/trabajar.js
+++ economia/trabajar.js
@@ -1,4 +1,4 @@
-const { EmbedBuilder } = require('discord.js'); // Asegurate de importar EmbedBuilder
+const { EmbedBuilder } = require('discord.js');
 const Enmap = require('enmap');
 const db = new Enmap({ name: "economia" });
 const { trabajos } = require('../data.js'); 
@@ -15,7 +15,13 @@ module.exports = {
             return message.reply('❌ No tenés un trabajo firmado. Usá `$trabajos firmar [nombre]` primero.');
         }
 
-        const trabajoData = trabajos.find(t => t.nombre.toLowerCase() === trabajoActual.toLowerCase());
+        // BUSCAMOS EL TRABAJO EXACTO COMPARANDO NOMBRES
+        const trabajoData = trabajos.find(t => t.nombre === trabajoActual);
+
+        if (!trabajoData) {
+            return message.reply('❌ Hubo un error al cargar tu empleo. Probá renunciar y volver a firmar.');
+        }
+
         const nivel = trabajos.indexOf(trabajoData) + 1; 
         const pagoBase = nivel * 150;
         const bonus = Math.floor(Math.random() * 200);
@@ -26,14 +32,13 @@ module.exports = {
 
         const totalTrabajos = db.get(userId, "trabajos");
         
-        // --- AQUÍ HACEMOS EL EMBED PERSONALIZADO ---
         const embedTrabajo = new EmbedBuilder()
-            .setColor(0xF1C40F) // Color naranja/amarillo como en la captura
+            .setColor(0xF1C40F)
             .setAuthor({ name: 'Trabajando', iconURL: 'https://cdn-icons-png.flaticon.com/512/2921/2921761.png' })
             .setDescription(`**${message.member}**\nTrabajás como **${trabajoActual}** y recibís **${ganancia}$**.\nTotal de trabajos: **${totalTrabajos}**`)
-            .setThumbnail(message.author.displayAvatarURL()); // Pone tu foto al costado como en la captura
+            .setThumbnail(message.author.displayAvatarURL());
 
         message.reply({ embeds: [embedTrabajo] });
     }
 };
-            
+        
