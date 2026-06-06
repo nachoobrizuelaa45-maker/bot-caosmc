e45677e1e1f036522f5e19b390aeb649fe332ee6
nachoobrizuelaa45-maker <nachoobrizuelaa45@gmail.com>
2026-06-06 19:27:16 -0300

    Update and rename dar-efectivo.js to dar-efectivos.js

--- economia/dar-efectivos.js
+++ economia/dar-efectivos.js
@@ -5,17 +5,17 @@ const { EmbedBuilder } = require('discord.js');
 module.exports = {
     name: 'dar-efectivo',
     async execute(message, args) {
-        // 1. IDs de los ROLES permitidos
+        // IDs de los ROLES permitidos
         const allowedRoles = ['1506013227686039562', '1509746102415392808'];
 
-        // 2. Verificar si el usuario tiene alguno de los roles permitidos
+        // Verificar si el usuario tiene alguno de los roles
         const hasRole = allowedRoles.some(roleId => message.member.roles.cache.has(roleId));
 
         if (!hasRole) {
             return message.reply('⛔️ No tenés el rol necesario para usar este comando.');
         }
 
-        // 3. Obtener datos
+        // Obtener datos (Primero el monto, segundo el usuario)
         const monto = parseInt(args[0]);
         const target = message.mentions.members.first();
 
@@ -23,7 +23,7 @@ module.exports = {
             return message.reply('⛔️ Uso correcto: `$dar-efectivo [monto] @usuario`');
         }
 
-        // 4. Sumar al efectivo del usuario
+        // Sumar al efectivo
         db.math(target.id, "add", monto, "dinero");
 
         const embed = new EmbedBuilder()
