015cf9dd7a0cacc375cdd206dd8dd3626dcb50ac
nachoobrizuelaa45-maker <nachoobrizuelaa45@gmail.com>
2026-06-07 10:44:37 -0300

    Update trabajar.js

--- economia/trabajar.js
+++ economia/trabajar.js
@@ -3,7 +3,6 @@ const db = require('../db');
 const { trabajos } = require('../data.js'); 
 const { esCanalValido } = require('./verificarCanal');
 
-// Objeto para manejar el tiempo de espera por usuario
 const cooldowns = new Map();
 
 module.exports = {
@@ -13,20 +12,20 @@ module.exports = {
         if (!esCanalValido(message)) return;
 
         const userId = message.author.id;
+        const user = message.author;
 
-        // 1. Lógica de Cooldown (5 segundos)
+        // 1. Cooldown de 5 segundos
         const ahora = Date.now();
-        const tiempoEspera = 5000; // 5 segundos
+        const tiempoEspera = 5000;
         if (cooldowns.has(userId)) {
             const tiempoRestante = cooldowns.get(userId) + tiempoEspera - ahora;
             if (tiempoRestante > 0) {
-                return message.reply(`⏳• Debes esperar **${Math.ceil(tiempoRestante / 1000)}** segundos para volver a trabajar.`)
-                    .then(msg => setTimeout(() => msg.delete(), 5000));
+                return message.reply(`⏳• Debes esperar **${Math.ceil(tiempoRestante / 1000)}** segundos.`).then(msg => setTimeout(() => msg.delete(), 5000));
             }
         }
         cooldowns.set(userId, ahora);
 
-        // 2. Base de datos
+        // 2. Lógica de trabajo
         db.ensure(userId, { dinero: 0, trabajos: 0, trabajoActual: 'Desempleado' });
         const trabajoActual = db.get(userId, "trabajoActual");
 
@@ -41,13 +40,16 @@ module.exports = {
         db.math(userId, "add", 1, "trabajos");
         db.math(userId, "add", ganancia, "dinero");
 
-        // 3. Embed estilo "Trabajando" igual a la imagen
+        // 3. Embed con diseño visual (Perfil a la izquierda y derecha)
         const embed = new EmbedBuilder()
-            .setAuthor({ name: '👷 Trabajando', iconURL: 'https://cdn-icons-png.flaticon.com/512/2910/2910165.png' })
-            .setColor(0xFF8C00) // Color naranja del borde de la imagen
-            .setDescription(`**<@${userId}>**\nTrabajas como **${trabajoActual}** y recibes **${ganancia}$**.`);
+            .setAuthor({ 
+                name: '👷 Trabajando', 
+                iconURL: user.displayAvatarURL() // Foto de perfil a la izquierda
+            })
+            .setDescription(`**@${user.username}**\nTrabajas como **${trabajoActual}** y recibes **${ganancia}$**.`)
+            .setThumbnail(user.displayAvatarURL()) // Foto de perfil a la derecha (redonda)
+            .setColor(0xFF8C00); // Color del borde lateral
 
         message.channel.send({ embeds: [embed] });
     }
 };
-                              
