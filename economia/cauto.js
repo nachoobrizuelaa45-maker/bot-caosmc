f090b1cb54a9cf3fd4fafb34da6ea41dff17ef1a
nachoobrizuelaa45-maker <nachoobrizuelaa45@gmail.com>
2026-06-19 20:32:25 -0300

    Update cauto.js

--- economia/cauto.js
+++ economia/cauto.js
@@ -1,11 +1,10 @@
 const { EmbedBuilder } = require('discord.js');
-const db = require('../db'); // Conexión centralizada al archivo db.js
-const { esCanalValido } = require('./verificarCanal'); // CORREGIDO: Ruta local
+const db = require('../db'); 
+const { esCanalValido } = require('./verificarCanal'); 
 
 module.exports = {
     name: 'cauto',
     async execute(message) {
-        // Borramos el comando original al instante
         message.delete().catch(() => {});
         
         if (!esCanalValido(message)) return;
@@ -13,8 +12,8 @@ module.exports = {
         const userId = message.author.id;
         const PRECIO_AUTO = 100000;
 
-        // Nos aseguramos de tener la estructura en la DB central
-        db.ensure(userId, { auto: 0, dinero: 0 });
+        // Aseguramos la estructura usando 'autos' (con S)
+        db.ensure(userId, { autos: 0, dinero: 0 });
         const dineroActual = db.get(userId, "dinero");
 
         // 1. Validación de saldo
@@ -23,8 +22,8 @@ module.exports = {
                 .then(msg => setTimeout(() => msg.delete(), 5000));
         }
 
-        // 2. Realizar compra en la DB central
-        db.math(userId, "add", 1, "auto");
+        // 2. Realizar compra usando la clave 'autos'
+        db.math(userId, "add", 1, "autos"); // <-- CORREGIDO: Ahora suma a 'autos'
         db.math(userId, "sub", PRECIO_AUTO, "dinero");
 
         const embed = new EmbedBuilder()
@@ -35,4 +34,3 @@ module.exports = {
         message.channel.send({ embeds: [embed] });
     }
 };
-
